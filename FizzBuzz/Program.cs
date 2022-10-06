316e0a79078aee01fd5f3fc155fa50fb9ba64fa0
Ben Cofield <benrcofield@gmail.com>
2022-10-06 13:15:15 -0400

    FizzBuzz

---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+for (int i = 1; i < 100; i++)
+{
+    string str = $"";
+
+    if (i % 3 == 0) str += "Fizz";
+
+    if (i % 5 == 0) str += "Buzz";
+    
+    if (str == "") Console.WriteLine(i);
+    else Console.WriteLine(str);
+}
